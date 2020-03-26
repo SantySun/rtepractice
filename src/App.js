b1d8c0cc9ea62bda6ca45a0efa8cc938430ec7e8
SantySun <sunjixiang.gz@gmail.com>
2020-03-26 23:15:47 +1100

    Init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { EditorState, convertToRaw } from 'draft-js';
+import { Editor } from 'react-draft-wysiwyg';
+import draftToHtml from 'draftjs-to-html';
+import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+
+class EditorConvertToHTML extends Component {
+  constructor() {
+    super();
+    this.state = {
+      editorState: EditorState.createEmpty(),
+    };
+    this.onEditorStateChange = this.onEditorStateChange.bind(this);
+    this.uploadImageCallBack = this.uploadImageCallBack.bind(this);
+  }
+
+  onEditorStateChange(editorState) {
+    this.setState({
+      editorState,
+    });
+  };
+
+  uploadImageCallBack(file) {
+    return new Promise(
+      (resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        xhr.open('POST', 'http://192.168.20.6:4000/api/file/');
+        const data = new FormData();
+        data.append('image', file);
+        xhr.send(data);
+        xhr.addEventListener('load', () => {
+          const response = JSON.parse(xhr.responseText);
+          resolve(response);
+        });
+        xhr.addEventListener('error', () => {
+          const error = JSON.parse(xhr.responseText);
+          reject(error);
+        });
+      }
+    );
+  }
+
+  render() {
+    const { editorState } = this.state;
+    return (
+      <div className="border container">
+        <Editor
+          editorState={editorState}
+          wrapperClassName="demo-wrapper"
+          editorClassName="demo-editor border"
+          onEditorStateChange={(editorState) => this.onEditorStateChange(editorState)}
+          toolbar={{
+            inline: { inDropdown: false },
+            list: { inDropdown: false },
+            textAlign: { inDropdown: false },
+            link: { inDropdown: false },
+            history: { inDropdown: true },
+            image: { uploadCallback: this.uploadImageCallBack, alt: { present: true, mandatory: false } },
+          }}
+        />
+        <p style={{ width: "auto" }}
+        // disabled
+        >{draftToHtml(convertToRaw(editorState.getCurrentContent()))}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      </div>
+    );
+  }
 }
-
-export default App;
+export default EditorConvertToHTML;
